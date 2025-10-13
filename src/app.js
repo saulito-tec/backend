5d7c8f773f6bc6301a0cf125c875cfed87cc77eb
Saúl <147526498+saulito-tec@users.noreply.github.com>
2025-10-12 22:52:24 -0600

    conflictos

--- src/app.js
+++ src/app.js
@@ -5,19 +5,12 @@ import morgan from 'morgan'
 // Importar rutas
 
 import authRoutes from './routes/auth.routes.js'
-// import usuariosRoutes from "./routes/usuarios.routes.js";
-<<<<<<< HEAD
-import productosRoutes from "./routes/productos.routes.js";
-// import inventarioRoutes from "./routes/inventario.routes.js";
-=======
-// import productosRoutes from "./routes/productos.routes.js";
-import inventarioRoutes from "./routes/inventario.routes.js";
->>>>>>> GH-54
-// import entradasRoutes from "./routes/entradas.routes.js";
+import usuariosRoutes from './routes/usuarios.routes.js'
+import productosRoutes from './routes/productos.routes.js'
+import inventarioRoutes from './routes/inventario.routes.js'
 import salidasRoutes from './routes/salidas.routes.js'
 import entradasRoutes from './routes/entradas.routes.js'
-// import salidasRoutes from "./routes/salidas.routes.js";
-// import reportesRoutes from "./routes/reportes.routes.js";
+import reportesRoutes from './routes/reportes.routes.js'
 
 const app = express()
 
@@ -30,19 +23,12 @@ app.use(morgan('dev'))
 // Prefijo global de la API
 
 app.use('/api/auth', authRoutes)
-// app.use("/api/usuarios", usuariosRoutes);
-<<<<<<< HEAD
-app.use("/api/productos", productosRoutes);
-// app.use("/api/inventario", inventarioRoutes);
-=======
-// app.use("/api/productos", productosRoutes);
-app.use("/api/inventario", inventarioRoutes);
->>>>>>> GH-54
-// app.use("/api/entradas", entradasRoutes);
+app.use('/api/usuarios', usuariosRoutes)
+app.use('/api/productos', productosRoutes)
+app.use('/api/inventario', inventarioRoutes)
 app.use('/api/salidas', salidasRoutes)
 app.use('/api/entradas', entradasRoutes)
-// app.use("/api/salidas", salidasRoutes);
-// app.use("/api/reportes", reportesRoutes);
+app.use('/api/reportes', reportesRoutes)
 
 // Ruta base de prueba
 app.get('/', (req, res) => {
